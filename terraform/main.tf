b0f8d928fc3b3223e0ac882889630f1edadfb765
Thanasis Politis <thanasis.politis@pcg.io>
2024-10-08 23:56:54 +0300

    Create sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "thanasis.politis+sandbox@hey.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    SSOUserEmail              = "thanasis.politis@hey.com"
+    SSOUserFirstName          = "Thanasis"
+    SSOUserLastName           = "Politis"
   }
 
   account_tags = {
